ac51fcc80e770908bf0ccc808399ba69958825a5
theArctesian <stephen.d.okita@gmail.com>
2024-01-26 17:01:51 -0800

    pee poo

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -41,12 +41,11 @@
       map.setMaxBounds(bounds);
       leaflet
         .tileLayer(
-         'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	          minZoom: 2,
-	          maxZoom: 10,
-	          attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	          ext: 'png' 
-         }
+        'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
+	minZoom: 2,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg' }
         )
         .addTo(map);
 
