4a1de161c0b4b707366889da30040d8559e20e54
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2024-04-11 01:41:11 -0700

    style changes

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -39,18 +39,15 @@
       map = leaflet.map(mapElement).setView([22.3, 114.16], 4);
 
       map.setMaxBounds(bounds);
-      leaflet
-        .tileLayer(
-          "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}",
-          {
-            minZoom: 1,
-            maxZoom: 16,
-            attribution:
-              '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            ext: "jpg",
-          }
-        )
-        .addTo(map);
+      leaflet;
+      var OpenStreetMap_Mapnik = L.tileLayer(
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
+      ).addTo(map);
 
       places.forEach((city) => {
         leaflet
