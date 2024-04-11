02f3c209da932300410d4e80e6a121be08932728
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2024-04-11 01:33:29 -0700

    pee pee poo poo

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -40,14 +40,12 @@
 
       map.setMaxBounds(bounds);
       leaflet
-        .tileLayer(
-          "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}",
-          {
-            minZoom: 2,
-            maxZoom: 16,
-            ext: "jpg",
-          }
-        )
+      .tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
+	minZoom: 1,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+}) 
         .addTo(map);
 
       places.forEach((city) => {
