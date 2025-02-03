b0357f310bda20a36395858bdda523a02ac3aabb
theArctesian <stephen.d.okita@gmail.com>
2025-02-02 20:39:45 -0800

    changed map

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -40,13 +40,15 @@
 
       map.setMaxBounds(bounds);
       leaflet;
+
       var OpenStreetMap_Mapnik = L.tileLayer(
-        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+        "https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}",
         {
           minZoom: 2,
           maxZoom: 16,
           attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          ext: "png",
         }
       ).addTo(map);
 
